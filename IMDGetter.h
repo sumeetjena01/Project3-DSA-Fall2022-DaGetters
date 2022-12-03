a5739b816e4a4eecd42ac10b4bc0bc18ddee4f5f
Sumeet <73847665+sumeetjena01@users.noreply.github.com>
2022-12-03 14:08:41 -0500

    Update IMDGetter.h

--- IMDGetter.h
+++ IMDGetter.h
@@ -30,11 +30,15 @@ public:
 
 private:
 
-    unordered_set<std::string> filterTypes = {"year", "genre", "runtime"}; // What is this for?
+    unordered_set<std::string> filterTypes = {"year", "genre", "runtime"}; // What is this for? - Sumeet
     unordered_set<std::string> sortTypes = {"title", "type", "year", "runTime"}; // Use these to validate input with find() or count()?
+    
     // We don't sort by type, right? We only need to sort by title (alphabetically), or year and runTime (numerically).
-    // The first question the user asks already filters out the type they're looking for.
-    // After that, the user is only trying to sort between those 3 things, title, year, and runtime.
+    // The first question the user asks already filters out the type of media they're looking for.
+    // After that, the user is only trying to sort between 3 things, either title, year, and runtime.
+    // If the user wants to see movies, all the movies will be loaded into a vector called "movies" (Line 134 in the IMDGetter.cpp file)
+    // From there, we just sort the vector using one of the two algorithms, right? 
+    // Sumeet's comments^^^
 
 };
 
