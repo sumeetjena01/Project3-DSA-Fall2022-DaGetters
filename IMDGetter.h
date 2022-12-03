f6a6f9214234b9020d8129202cc26c72777bfbaf
Sumeet <73847665+sumeetjena01@users.noreply.github.com>
2022-12-02 22:40:49 -0500

    Update IMDGetter.h

--- IMDGetter.h
+++ IMDGetter.h
@@ -2,7 +2,11 @@
 #define PROJECT3AMENU_IMDGETTER_H
 #endif //PROJECT3AMENU_IMDGETTER_H
 #include <iostream>
+#include <vector>
 #include <unordered_set>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 class Media {
@@ -10,6 +14,7 @@ class Media {
 public:
 
     // Members
+    string tConst;
     string titleType;
     string primaryTitle;
     bool isAdult;
@@ -21,16 +26,80 @@ public:
     // Functions
     void checkIfAdult(int input);
     void PrintMenu();
+    static bool LoadTSVFile(string filename, vector<Media*> &list);
+    static Media* CreateMediaData(string line);
+
 
 private:
-    std::unordered_set<std::string> filterTypes = {"year", "genre", "runtime"};
-    std::unordered_set<std::string> sortTypes = {"title", "type", "year", "runTime"}; //use these to validate input with find() or count()?
+
+    unordered_set<std::string> filterTypes = {"year", "genre", "runtime"};
+    unordered_set<std::string> sortTypes = {"title", "type", "year", "runTime"}; //Use these to validate input with find() or count()?
 
 };
 
 void Media::checkIfAdult (int input) {
     // If the adult column is 1, don't read in input.
     // Otherwise, read in the input.
+}
+
+// After that^ is done, pass the data into a vector, to be sorted later.
+
+bool Media::LoadTSVFile(string filename, vector<Media*> &list) {
+
+    ifstream inputFile(filename);
+    if(!inputFile.is_open()) {
+        cout << "The file is not open." << endl;
+    }
+    string headerLine;
+    getline(inputFile, headerLine);
+
+    string dataLine;
+    while (inputFile.is_open() && !inputFile.eof()) {
+        getline(inputFile, dataLine);
+        Media* mediaData = CreateMediaData(dataLine);
+        list.push_back(mediaData);
+    }
+    inputFile.close();
+    return true;
+
+}
+
+Media* Media::CreateMediaData(string line) {
+
+    Media* mediaData = new Media();
+    stringstream sStream(line);
+
+    while(sStream.good()) {
+        string tConst;
+        getline(sStream, tConst, '\t');
+
+        string titleType;
+        getline(sStream, titleType, '\t');
+
+        string primaryTitle;
+        getline(sStream, primaryTitle, '\t');
+
+        string startYear;
+        getline(sStream, startYear, '\t');
+        int intStartYear = stoi(startYear);
+
+        string endYear;
+        getline(sStream, endYear, '\t');
+        int intEndYear = stoi(endYear);
+
+        string runtime;
+        getline(sStream, runtime, '\t');
+        int realRuntime = stoi(runtime);
+
+        mediaData->tConst = tConst;
+        mediaData->titleType = titleType;
+        mediaData->primaryTitle = primaryTitle;
+        mediaData->startYear = startYear;
+        mediaData->endYear = endYear;
+        // mediaData->runtime = runtime; *Have to fix.* 
+
+    }
+    return mediaData;
 }
 
 void Media::PrintMenu() {
@@ -50,8 +119,8 @@ void Media::PrintMenu() {
         cout << "Please enter a valid input. If needed, refer the menu above. " << endl;
         cin >> searchInput;
     }
-    // When the user selects one of the options, pass all of those options from the .XSLM file into a vector.
-    // EX: If the user types in 1, pass all the movies from the .XSLM file into a vector to be sorted later.
+    // When the user selects one of the options, pass all of those options from the .TSV file into a vector.
+    // EX: If the user types in 1, pass all the movies from the .TSV file into a vector to be sorted later.
     // But, within that function, if the movie has a 1 in the adult column, do not read that into the vector.
 
     cout << "What would you like to sort by?" << endl;
@@ -64,6 +133,8 @@ void Media::PrintMenu() {
         cout << "Please enter a valid input. If needed, refer the menu above. " << endl;
         cin >> sortInput;
     }
+
+    // If they pick 1, meaning they want to see movies, should I sort the vector here?
 
     cout << "How would you like to perform your search?" << endl;
     cout << "1. Quick Sort" << endl;
@@ -95,6 +166,3 @@ void Media::PrintMenu() {
         }
     }
 }
-
-
-
