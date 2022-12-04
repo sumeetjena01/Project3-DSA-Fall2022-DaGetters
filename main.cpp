ecf6e554e6f8f7046cfd18b2f1b0b5a1548cfe9d
Sumeet <73847665+sumeetjena01@users.noreply.github.com>
2022-12-04 09:49:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main() {
 
     IMDBData imdbGetter; // Object
-    imdbGetter.LoadTSVFile("testload.tsv");
-    // imdbGetter.LoadTSVFile("title.basics.tsv");
+    imdbGetter.LoadTSVFile("title.basics.tsv");
     imdbGetter.PrintMenu();
     return 0;
 }
 
 /*
- The purpose of the project is to take in a large dataset, and sort it using two algorithms, and compare the times.
 
- So, if the user wants to see a list of all the movies on IMDb, I have to read in all the movies from the .TSV file, and put them into a vector.
- From there, we will sort the movies
- 
+ The purpose of the project is to take in a large dataset, and sort it using two algorithms, and compare the times.
+ So, if the user wants to see a list of all the movies on IMDb, we have to read in all the movies from the .TSV file, and put them into a vector.
+ From there, we will sort the movies using sorting algorithms.
+
  */
